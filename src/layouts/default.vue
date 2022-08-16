00cd944f4c795b728c5b9e3a8e32da8fbc9f8d2d
Vitaliy Lyovkin <unf.hwm@ya.ru>
2022-08-16 23:13:45 +0700

    favicon

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -3,10 +3,10 @@
     data-test="default-layout"
     class="px-6 container mx-auto h-full w-full flex flex-col"
   >
-    <application-base-header class="sticky top-0 left-0 p-2" />
+    <ApplicationBaseHeader class="sticky top-0 left-0 p-2" />
 
     <main class="h-full w-full"><slot /></main>
 
-    <application-base-footer class="mt-24 pb-8" />
+    <ApplicationBaseFooter class="mt-24 pb-8" />
   </div>
 </template>
