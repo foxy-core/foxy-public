46133b9442cb2e79200f910633a60e1c232c8997
Vitaliy Lyovkin <unf.hwm@ya.ru>
2022-08-24 02:21:37 +0700

    fix: skip hydration for cookies

--- src/_app/stores/credentials.store.ts
+++ src/_app/stores/credentials.store.ts
@@ -1,24 +1,32 @@
-import { defineStore } from 'pinia'
+import { defineStore, skipHydrate } from 'pinia'
 
 import { AuthorizationCookies } from '~/_app/domain/auth'
 import { INFINITE_MAX_AGE } from '~/_app/common/http-utils'
 
 export const useCredentialsStore = defineStore('credentials', () => {
-  const accessToken = useCookie(AuthorizationCookies.AccessToken, {
-    maxAge: INFINITE_MAX_AGE,
-  })
+  const accessToken = skipHydrate(
+    useCookie(AuthorizationCookies.AccessToken, {
+      maxAge: INFINITE_MAX_AGE,
+    }),
+  )
 
-  const clientId = useCookie(AuthorizationCookies.ClientId, {
-    maxAge: INFINITE_MAX_AGE,
-  })
+  const clientId = skipHydrate(
+    useCookie(AuthorizationCookies.ClientId, {
+      maxAge: INFINITE_MAX_AGE,
+    }),
+  )
 
-  const refreshToken = useCookie(AuthorizationCookies.RefreshToken, {
-    maxAge: INFINITE_MAX_AGE,
-  })
+  const refreshToken = skipHydrate(
+    useCookie(AuthorizationCookies.RefreshToken, {
+      maxAge: INFINITE_MAX_AGE,
+    }),
+  )
 
-  const tokenValidity = useCookie<boolean>(AuthorizationCookies.TokenValidity, {
-    maxAge: 3_600,
-  })
+  const tokenValidity = skipHydrate(
+    useCookie<boolean>(AuthorizationCookies.TokenValidity, {
+      maxAge: 3_600,
+    }),
+  )
 
   return {
     accessToken,
